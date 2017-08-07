78b74510ab76e1475e135391ac96179e55531362
Lucas Siqueira <lucassouza.ti@gmail.com>
2017-08-07 01:14:30 -0300

    implementado remocao de stopwords e pontuacao

--- fracionamentos.py
+++ fracionamentos.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 import requests, json
-from nltk import tokenize
-# import nltk
+# from nltk import tokenize
+import nltk
+# from nltk.tokenize import RegexpTokenizer
 from collections import defaultdict
 
 def map_reduce(inputs):
 	collector = defaultdict(list)
-	# print(inputs)
+
 	for input in inputs:
 		for key, value in mapper(input):
 			collector[key].append(value)
@@ -17,22 +18,25 @@ def map_reduce(inputs):
 			for output in reducer(key,values)]
 
 def mapper(document):
-	for word in document.split(' '):
-		print(word)
-		yield (word, 1)
+	stopwords = nltk.corpus.stopwords.words('portuguese')
+	punctuations = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
+	# toker = RegexpTokenizer(r'((?<=[^\w\s])\w(?=[^\w\s])|(\W))+', gaps=True)
+	for word in nltk.word_tokenize(document):
+		if word not in stopwords and word not in punctuations:
+			yield (word, 1)
 
 def reducer(word, counts):
-	# print(word)
 	yield (word, sum(counts))
 
-endpoint = "http://slacs.dev/api/contratos/itens"
+endpoint = "http://slacs.dev/api/contratos"
 saida = json.loads(requests.get(endpoint).text)
 # print saida[0]['descricao']
 valores = set()
 for item in saida:
+	print(item['descricao'])
 	valores.add(item['descricao'])
 	
 print(sorted(map_reduce(valores),
 	key=lambda (word, count): count,
 	reverse=True))
-
+# print(nltk.corpus.stopwords.words('portuguese'))
